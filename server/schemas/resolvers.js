18e538c808b3cf4638d45e5d98620517ebafb887
thossain <tanvirhossain2006@gmail.com>
2021-10-31 23:52:20 +1100

    added 2 mutation in resolver

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,42 @@
+const { AuthenticationError } = require('apollo-server-express');
+const {
+    User, 
+    Book, 
+    Marker, 
+    BooksRating, 
+    Category, 
+    Comment, 
+    CommentRating 
+} = require('../models');
+
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query:{
+
+    },
+    Mutation:{
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('No user found with this email address');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+    },
+}
--- server/server.js
+++ server/server.js
@@ -2,17 +2,17 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-// const { typeDefs, resolvers } = require('./schemas');
-// const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 // const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware
-// });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 server.applyMiddleware({ app });
 
