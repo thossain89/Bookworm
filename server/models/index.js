8834385d0242275d1b309b8bc3855e3f7f53bdab
thossain <tanvirhossain2006@gmail.com>
2021-11-01 12:45:31 +1100

    typeDef and resolvers Upgraded

--- server/models/index.js
+++ server/models/index.js
@@ -4,6 +4,6 @@ const Marker = require('./Marker');
 const BooksRating = require('./BookRating');
 const Category = require('./Category');
 const Comment = require('./Comment');
-const CommentRating = require('./CommentRating');
 
-module.exports = {User, Book, Marker, BooksRating, Category, Comment, CommentRating};
+
+module.exports = {User, Book, Marker, BooksRating, Category, Comment};
