d0cf22cacad3e1df114df2095a794a1a535c2933
thossain <tanvirhossain2006@gmail.com>
2021-11-01 18:54:31 +1100

    resolver query updated

--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,9 @@
 const User = require('./User');
 const Book = require('./Book');
 const Marker = require('./Marker');
-const BooksRating = require('./BookRating');
+const BookRating = require('./BookRating');
 const Category = require('./Category');
 const Comment = require('./Comment');
 
 
-module.exports = {User, Book, Marker, BooksRating, Category, Comment};
+module.exports = {User, Book, Marker, BookRating, Category, Comment};
